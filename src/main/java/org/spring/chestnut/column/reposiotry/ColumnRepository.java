ef67c4ef0bacd29996bae1ee9137036760bdf78e
YOOSUNAH <tjsdkyd4027@gmail.com>
2024-03-19 21:59:00 +0900

    :recycle: 사용하지 않는 부분 삭제

--- src/main/java/org/spring/chestnut/column/reposiotry/ColumnRepository.java
+++ src/main/java/org/spring/chestnut/column/reposiotry/ColumnRepository.java
@@ -1,7 +1,6 @@
 package org.spring.chestnut.column.reposiotry;
 
 import java.util.List;
-import java.util.Optional;
 import org.spring.chestnut.column.entity.ColumnEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -9,6 +8,7 @@ public interface ColumnRepository extends JpaRepository<ColumnEntity, Long> {
 
     Integer countByBoardId(Long boardId);
 
+    List<ColumnEntity> findAllByBoardId(Long boardId);
     List<ColumnEntity> findBySequenceBetween(int i, Integer newSequence);
 
 
