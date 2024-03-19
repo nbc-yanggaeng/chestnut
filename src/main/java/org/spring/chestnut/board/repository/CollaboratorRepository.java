38466dfbadc0de9876cb3d6b4678fcdbebbd4c20
tktcod <hs060100@gmail.com>
2024-03-19 20:24:35 +0900

    feat: CollaboratorEntity 추가 보드 생성시 생성 유저 Collaborator 에 추가

---
+++ src/main/java/org/spring/chestnut/board/repository/CollaboratorRepository.java
@@ -0,0 +1,8 @@
+package org.spring.chestnut.board.repository;
+
+import org.spring.chestnut.board.entity.CollaboratorEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CollaboratorRepository extends JpaRepository<CollaboratorEntity, Long> {
+
+}
