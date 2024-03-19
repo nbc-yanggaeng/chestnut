9e952ba7542d375b2040f758326193a21842c2fb
gloz0315 <qudtn0315@naver.com>
2024-03-19 13:37:20 +0900

    feat(member): 회원 가입에 대한 dto 생성

---
+++ src/main/java/org/spring/chestnut/member/dto/request/SignupDto.java
@@ -0,0 +1,15 @@
+package org.spring.chestnut.member.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class SignupDto {
+
+  private final String email;
+  private final String password;
+
+  public SignupDto(String email, String password) {
+    this.email = email;
+    this.password = password;
+  }
+}
---
+++ src/main/java/org/spring/chestnut/member/dto/response/MemberResponseDto.java
@@ -0,0 +1,13 @@
+package org.spring.chestnut.member.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class MemberResponseDto {
+
+  private final String email;
+
+  public MemberResponseDto(String email) {
+    this.email = email;
+  }
+}
